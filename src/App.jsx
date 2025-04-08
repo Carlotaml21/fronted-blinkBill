33c24a360ec4d6c7cc880a284e89289fe2df0548
Carlotaml21 <carlotaml21@gmail.com>
2025-04-08 18:39:09 +0200

    add about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from './components/Header';
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage/>
+      <AboutPage/>
       <Footer />
     </>
   );
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center px-6 py-12">
+      <h1 className="text-3xl font-bold text-[#26BFBF] mb-8 text-center">Sobre Nosotros</h1>
+
+      <div className="max-w-3xl space-y-6 text-center text-[#1D3440]">
+        <p>
+          En <span className="text-[#26BFBF]"> BlinkBill</span>, creemos en la transparencia y accesibilidad de la información empresarial. 
+          Nuestra plataforma te permite consultar libremente los datos de facturación de empresas de manera rápida y sencilla.
+        </p>
+
+        <p>
+          Además, si eres empresario o autónomo, puedes añadir tu propia empresa y actualizar su información en cualquier momento, asegurando que los datos sean siempre precisos y relevantes.
+        </p>
+
+        <p>
+          Ya seas un profesional en busca de referencias comerciales o una empresa que quiere mejorar su visibilidad, 
+          <span className="text-[#26BFBF]"> BlinkBill</span> te ofrece una solución ágil y abierta para gestionar la información de facturación en un solo lugar.
+        </p>
+      </div>
+
+      <p className="mt-12 text-center text-[#1D3440] font-bold">
+        Únete a nuestra <span className="text-[#26BFBF]">comunidad</span> y descubre cómo la transparencia puede impulsar tu negocio.
+      </p>
+
+      <div className="mt-8">
+        <img 
+          src="/logoBlinkBill.png" // Asegúrate de tener esta imagen en la carpeta pública o cambiar la ruta
+          alt="BlinkBill logo" 
+          className="w-24 h-24 mx-auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
