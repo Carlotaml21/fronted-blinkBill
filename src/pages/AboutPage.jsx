bcc743316d8782507680e5589cca2fe38464f530
Carlotaml21 <carlotaml21@gmail.com>
2025-04-23 14:57:44 +0200

    modify text

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const AboutPage = () => {
   return (
     <div className="min-h-screen bg-white flex flex-col items-center justify-center px-6 py-12">
-      <h1 className="text-3xl font-bold text-[#26BFBF] mb-8 text-center">Sobre Nosotros</h1>
+      <h1 className="text-5xl font-bold text-[#26BFBF] mb-8 text-center">Sobre Nosotros</h1>
 
-      <div className="max-w-3xl space-y-6 text-center text-[#1D3440]">
+      <div className="max-w-3xl space-y-6 text-center text-xl text-[#1D3440]">
         <p>
           En <span className="text-[#26BFBF]"> BlinkBill</span>, creemos en la transparencia y accesibilidad de la información empresarial. 
           Nuestra plataforma te permite consultar libremente los datos de facturación de empresas de manera rápida y sencilla.
@@ -21,7 +21,7 @@ const AboutPage = () => {
         </p>
       </div>
 
-      <p className="mt-12 text-center text-[#1D3440] font-bold">
+      <p className="mt-12 text-center text-[#1D3440] font-bold text-xl">
         Únete a nuestra <span className="text-[#26BFBF]">comunidad</span> y descubre cómo la transparencia puede impulsar tu negocio.
       </p>
 
@@ -29,7 +29,7 @@ const AboutPage = () => {
         <img 
           src="/src/assets/logoBlinkBill.png" 
           alt="BlinkBill logo" 
-          className="w-24 h-24 mx-auto"
+          className="w-20 h-20 mx-auto"
         />
       </div>
     </div>
